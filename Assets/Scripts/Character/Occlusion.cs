5e7ac342b4663086628e6922ceacf7425b44964e
EmeryYe <1339942935@qq.com>
2019-06-09 14:07:04 +0800

    更新NPC

---
+++ Assets/Scripts/Character/Occlusion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Occlusion : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Contains("NPC") || collision.tag.Contains("Item_Collision"))
+        {
+            collision.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0.5f);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag.Contains("NPC") || collision.tag.Contains("Item_Collision"))
+        {
+            collision.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f);
+        }
+    }
+}
