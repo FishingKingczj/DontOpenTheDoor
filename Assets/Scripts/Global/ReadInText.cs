ab03b3903030351454a7ebb47dba60deb3b4a7e1
EmeryYe <1339942935@qq.com>
2019-06-10 21:14:07 +0800

    更新

---
+++ Assets/Scripts/Global/ReadInText.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public static class ReadInText
+{
+    public static string[] readin(string path) {
+        var input = Resources.Load(path).ToString();
+        string[] lines = input.Split(new char[3] { '\r', '\n', ' ' });
+        var i = 0;
+        foreach (var line in lines)
+            if (line != "") i++;
+        string[] newlines = new string[i];
+        i = 0;
+        foreach (var line in lines)
+            if (line != "")
+            {
+                newlines[i] = line;
+                i++;
+            }
+        return newlines;
+    }
+}
